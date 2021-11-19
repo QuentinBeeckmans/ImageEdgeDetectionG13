70b03a6a81482525bfb91bfb26362effb501a3f3
mathieukohl <mathieu.kohl@students.hevs.ch>
2021-11-19 11:41:01 +0100

    Program 2
    first commit

---
+++ ImageEdgeDetectionTest/FilterTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImageEdgeDetection;
+using System.Drawing;
+
+namespace ImageEdgeDetectionTest
+{
+    [TestClass]
+    public class FilterTest
+    {
+        private const string FILE_URI = "C:\\Users\\mathi\\OneDrive\\Desktop\\CK-ECOM\\NFT\\orca.png"
+        private Bitmap originalBitmap = new Bitmap(FILE_URI);
+
+        [TestMethod]
+        public void ApplySwapFilterTest()
+        {
+            
+        }
+    }
+}
