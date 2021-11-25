06b91fee97be6c3ba1ad66d80dc8b2d808632606
mathieukohl <mathieu.kohl@students.hevs.ch>
2021-11-25 18:10:24 +0100

    Program 2
    Basic of tests

--- ImageEdgeDetectionTest/FilterTest.cs
+++ ImageEdgeDetectionTest/FilterTest.cs
@@ -1,13 +1,15 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ImageEdgeDetection;
 using System.Drawing;
+using ImageEdgeDetection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
 
 namespace ImageEdgeDetectionTest
 {
     [TestClass]
     public class FilterTest
     {
-        private const string FILE_URI = "C:\\Users\\mathi\\OneDrive\\Desktop\\CK-ECOM\\NFT\\orca.png"
+        private const string FILE_URI = "C:\\Users\\mathi\\OneDrive\\Desktop\\CK-ECOM\\NFT\\orca.png";
         private Bitmap originalBitmap = new Bitmap(FILE_URI);
 
         [TestMethod]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+using ImageEdgeDetection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string FILE_URI = "C:\\img\\orca.png";
+        private Bitmap originalBitmap = new Bitmap(FILE_URI);
+
+        [TestMethod]
+        public void ApplyCrazyFilterIsNotNull()
+        {
+            Bitmap bitmap = ExtBitmap.ApplySwapFilter(originalBitmap);
+            Assert.IsNotNull(bitmap);
+        }
+
+        [TestMethod]
+        public void ApplyCrazyFilterCorrectColors()
+        {
+            Bitmap bitmapResult = ExtBitmap.ApplyCrazyFilter(originalBitmap);
+            int alpha = 1;
+            int red = 1;
+            int blue = 2;
+            int green = 1;
+
+            for (int i = 0; i < originalBitmap.Width; i++)
+                for (int x = 0; x < originalBitmap.Height; x++)
+                {
+                    Color cOriginal = originalBitmap.GetPixel(i, x);
+                    Color cResult = bitmapResult.GetPixel(i, x);
+                    Assert.AreEqual(cResult.A, cOriginal.A / alpha);
+                    Assert.AreEqual(cResult.R, cOriginal.R / red);
+                    Assert.AreEqual(cResult.B, cOriginal.B / blue);
+                    Assert.AreEqual(cResult.G, cOriginal.G / green);
+                }
+        }
+
+        [TestMethod]
+        public void ApplySwapFilterCorrectColors()
+        {
+            Bitmap resultBitmap = ExtBitmap.ApplyFilterSwap(originalBitmap);
+            /*
+            int alpha = 1;
+            int red = 1;
+            int blue = 2;
+            int green = 1;
+            */
+
+            for (int i = 0; i < originalBitmap.Width; i++)
+                for (int x = 0; x < originalBitmap.Height; x++)
+                {
+                    Color cOriginal = originalBitmap.GetPixel(i, x);
+                    Color cResult = resultBitmap.GetPixel(i, x);
+                    Assert.AreEqual(cResult.A, cOriginal.A);
+                    Assert.AreEqual(cResult.G, cOriginal.G);
+                    Assert.AreEqual(cResult.B, cOriginal.B);
+                    Assert.AreEqual(cResult.R, cOriginal.R);
+                    
+                }
+        }
+    }
+}
